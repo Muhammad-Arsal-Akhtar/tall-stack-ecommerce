2ac35d4bf2a7a9a274c53237b2f2a213c82d6cd6
ArsalGitHub <arsal3414@gmail.com>
2024-09-25 15:04:20 +0500

    set the maganeg category table component and add its routes to the side bar

--- resources/views/admin-layout.blade.php
+++ resources/views/admin-layout.blade.php
@@ -116,6 +116,15 @@
                                         </svg>
                                         Products
                                     </a></li>
+                                    <li><a class="w-full flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-300 {{ request()->is('admin/category') ? 'bg-gray-100' : '' }}"
+                                        href="{{ url('admin/category') }}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+                                          </svg>
+                                          
+                                        Category
+                                </a></li>
                                 <li><a class="w-full flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-300 {{ request()->is('admin/orders') ? 'bg-gray-100' : '' }}"
                                         href="{{ url('admin/orders') }}">
                                         <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\ProductDetails;
 use App\Livewire\AdminDashboard;
 use App\Livewire\ManageProduct;
 use App\Livewire\ManageOrder;
+use App\Livewire\ManageCategory;
 use App\Livewire\AddProductForm;
 
 Route::get('/', function () {
@@ -14,13 +15,12 @@ Route::get('/', function () {
 Route::get('product-details', ProductDetails::class);
 
 
-
-
 Route::middleware(['admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', AdminDashboard::class)->name('dashboard');
     Route::get('/products', ManageProduct::class)->name('products');
     Route::get('/orders', ManageOrder::class)->name('orders');
     Route::get('/add/product', AddProductForm::class);
+    Route::get('/category', ManageCategory::class)->name('category');
 
 });
 
