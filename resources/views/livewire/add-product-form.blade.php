e7a9e0d5c61c480dd0878620bfa2e19e6ba28644
ArsalGitHub <arsal3414@gmail.com>
2024-09-25 14:46:58 +0500

    remove the useless class from add product form page

--- resources/views/livewire/add-product-form.blade.php
+++ resources/views/livewire/add-product-form.blade.php
@@ -104,7 +104,7 @@
                             x-on:livewire-upload-progress="progress = $event.detail.progress">
                             <label for="af-submit-application-resume-cv" class="sr-only">Choose file</label>
                             <input type="file" wire:model="photo" id="af-submit-application-resume-cv"
-                                class="block w-full text-sm border border-gray-200 rounded-lg shadow-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 file:bg-gray-50 file:border-0 file:bg-gray-100 file:me-4 file:py-2 file:px-4 dark:file:bg-neutral-700 dark:file:text-neutral-400">
+                                class="block w-full text-sm border border-gray-200 rounded-lg shadow-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 file:border-0 file:bg-gray-100 file:me-4 file:py-2 file:px-4 dark:file:bg-neutral-700 dark:file:text-neutral-400">
                                 @error('photo')
                                   <span class="text-red-500">{{ $message }}</span>
                                 @enderror
