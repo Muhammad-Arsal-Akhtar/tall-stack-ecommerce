2c5c1ad66f4626ca6e0dcdbf070d01cff9fb7562
ArsalGitHub <arsal3414@gmail.com>
2024-09-27 10:28:23 +0500

    apply lazy in the search filed in the listing table of products

--- resources/views/livewire/product-table.blade.php
+++ resources/views/livewire/product-table.blade.php
@@ -23,7 +23,7 @@
                             <div>
                                 <div class="inline-flex gap-x-2">
                                     <div class="max-w-sm space-y-3">
-                                        <input wire:model="search" type="text" class="block w-full px-4 py-3 text-sm border-gray-200 rounded-lg focus:border-teal-500 focus:ring-teal-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search Product" />
+                                        <input wire:model.lazy="search" type="text" class="block w-full px-4 py-3 text-sm border-gray-200 rounded-lg focus:border-teal-500 focus:ring-teal-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search Product" />
                                     </div>
 
                                     <a class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-teal-600 border border-transparent rounded-lg gap-x-2 hover:bg-teal-700 focus:outline-none focus:bg-teal-700 disabled:opacity-50 disabled:pointer-events-none"
@@ -170,7 +170,7 @@
                         <div
                             class="grid gap-3 px-6 py-4 border-t border-gray-200 md:flex md:justify-between md:items-center dark:border-neutral-700">
                             <div class="max-w-sm space-y-3">
-                                <select class="py-2 px-3 pe-9 block border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400" wire:model="perPage">
+                                <select class="block px-3 py-2 text-sm border-gray-200 rounded-lg pe-9 focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400" wire:model="perPage">
                                   <option value="2">2</option>
                                   <option value="3">3</option>
                                   <option value="5">5</option>
