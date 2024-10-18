ba5c7a9b9e1b2b0091b645bfe1917c80c58a65a3
ArsalGitHub <arsal3414@gmail.com>
2024-10-18 12:34:59 +0500

    changes the user model from dev dojo classes to breze officeal class and set the previous routes in routes file

--- routes/web.php
+++ routes/web.php
@@ -33,12 +33,12 @@ Route::middleware(['admin'])->prefix('admin')->group(function () {
 });
 
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+// Route::view('profile', 'profile')
+//     ->middleware(['auth'])
+//     ->name('profile');
 
 require __DIR__.'/auth.php';
