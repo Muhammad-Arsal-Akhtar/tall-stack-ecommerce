c708c64b0e204a4794cb4a392c7c5afa84ce4b4c
ArsalGitHub <arsal3414@gmail.com>
2024-10-18 11:09:02 +0500

    set the previous routes in the routes file and make hte home page to show

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Tall Stack Ecommerce</title>
+
+        @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
+
+    </head>
+    <body>
+        <livewire:header />
+        <livewire:hero-section />
+        <livewire:product-section />
+        <livewire:footer />
+    </body>
+</html>
